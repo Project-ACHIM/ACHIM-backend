b04a17e6e8807276fdfee636725f741659ab5981
zaiudon <2230360@ecc.ac.jp>
2025-06-20 01:41:36 +0000

    user関連のmodelsを定義 init、base、commonの追加

---
+++ backend/db/models/sp_record.py
@@ -0,0 +1,15 @@
+from db.models.common import *
+
+# --- sp_records ---
+class SPRecord(Base):
+    __tablename__ = "sp_records"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    week_id = Column(Integer, ForeignKey("weeks.id"))
+    date = Column(Date, nullable=False)
+    sp = Column(Integer, nullable=False)
+    detail = Column(JSON)
+    created_at = Column(DateTime, server_default=func.now())
+
+    user = relationship("User", back_populates="sp_records")
