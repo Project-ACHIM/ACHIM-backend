414b6fb9c2745b2887688ab22c707ec9d1b78170
2230142 <2230142@ecc.ac.jp>
2025-06-29 16:07:05 +0000

    インポートパスの統一とlog見れるように

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
-from api.auth import mail
-from api import users
+from backend.api.auth import mail
+from backend.api import users
+from backend.db.base import Base
+from backend.db.session import engine
+from backend.db import models  # モデル定義の読み込み
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 app = FastAPI()
 
 app.include_router(mail.router, prefix="/auth/mail", tags=["auth:mail"])
 app.include_router(users.router, prefix="/users", tags=["users"])
 
-
-
 # ひとまずテーブルを作るための処理
-from db.session import engine
-from db.models.common import Base  # モデル全体が登録されたBase
-
 print("テーブル作成開始")
 Base.metadata.create_all(bind=engine)
 print("テーブル作成完了")
-
