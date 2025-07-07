cdb9d5697a2c225343df2d17436ebb583c9464f0
zaiudon <2230360@ecc.ac.jp>
2025-07-07 06:09:10 +0000

    自動実行

--- backend/main.py
+++ backend/main.py
@@ -5,21 +5,18 @@ from backend.db.base import Base
 from backend.db.session import engine
 from backend.db import models  # モデル定義の読み込み
 from backend.api import auth_google
-
-
-# from fastapi import FastAPI, Depends
-# from sqlalchemy.orm import Session
-# from sqlalchemy import text
-# from db.session import get_db
-# from api import auth_google
-# from api.auth import mail
-# from api import users
-
+from backend.service.weekly_tasks import run_weekly_tasks
+from apscheduler.schedulers.background import BackgroundScheduler
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI()
+
+scheduler = BackgroundScheduler()
+# 毎週月曜0:10に実行
+scheduler.add_job(run_weekly_tasks, 'cron', day_of_week='mon', hour=0, minute=10)
+scheduler.start()
 
 app.include_router(mail.router, prefix="/auth/mail", tags=["auth:mail"])
 app.include_router(users.router, prefix="/users", tags=["users"])
