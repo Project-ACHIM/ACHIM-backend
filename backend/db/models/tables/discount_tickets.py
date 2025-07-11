5a8845f9393e80273399bf6a1ecf7a544009e7f6
tomo18058 <tomontomon1203@icloud.com>
2025-07-11 03:16:04 +0000

    インポート文訂正

--- backend/db/models/tables/discount_tickets.py
+++ backend/db/models/tables/discount_tickets.py
@@ -1,7 +1,7 @@
 from db.models.common import *
 from sqlalchemy import Column, Integer, String, Text, DateTime
 from sqlalchemy.sql import func
-# from backend.db.database import Base
+from db.session import Base
 
 # 割引券テーブルのモデル
 class DiscountTicket(Base):
