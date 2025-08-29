3b1ca46009b272b4590e603f45fc2131764a068e
2230142 <2230142@ecc.ac.jp>
2025-08-29 07:55:56 +0000

    App.Exception.messageの未定義

--- backend/core/errors.py
+++ backend/core/errors.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException, status
 
 class AppException(HTTPException):
     def __init__(self, status_code: int, message: str):
+        self.message = message
         super().__init__(status_code=status_code, detail=message)
 
 class NotFoundException(AppException):
